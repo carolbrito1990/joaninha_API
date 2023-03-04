1311aea1e02df018ff2fe6df5d97ba452e8d8376
carolina.brito <carolcsi1990@gmail.com>
2023-03-04 20:36:24 -0300

    Primeiro commit de 'verdade'

---
+++ src/main/java/br/com/springboot/joaninha_API/JoaninhaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.joaninha_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaninhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaninhaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/joaninha_API/JoaninhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.joaninha_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaninhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
